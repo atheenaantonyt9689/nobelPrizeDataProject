6cfd21692e77bc1dec998cb1102c5b71b3842bfc
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-05-31 14:19:22 +0530

    nobel prize count

--- prize/views.py
+++ prize/views.py
@@ -1,15 +1,24 @@
+from django.db.models.aggregates import Count
 from django.shortcuts import render
 from django.views import View
-from .models import Laureate
+from .models import Laureate, NobelPrize
 # Create your views here
 #from django.views.generic import TemplateView
+from django.db.models import Sum
 
 class HomePageView(View):
-    model=Laureate
+    model=Laureate,NobelPrize
     template_name='core_section/home.html'
     def get(self,request,*args,**kwargs): 
-        context={
+        
             
+        data =NobelPrize.objects.values('category','laureate__born_country' ).annotate(Count('category'))  
+        
+            
+        
+        context={
+           "data":data
+
         }
         
         return render(request,"core_section/home.html",context)
