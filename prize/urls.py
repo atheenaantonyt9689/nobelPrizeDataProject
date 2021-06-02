3ddd1833d7ff457ece2659e3acadc64e84708b6d
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-06-02 14:09:11 +0530

    nobelPrize counts based on gender & affln partially compltd

--- prize/urls.py
+++ prize/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView,NobelPrizeGender,AffiliationCount
 urlpatterns = [
 path('', HomePageView.as_view(), name='home'),
+path('gender_count', NobelPrizeGender.as_view(), name='gender'),
+path('affiliation_count', AffiliationCount.as_view(), name='affiliation')
 ]
--- prize/views.py
+++ prize/views.py
@@ -12,7 +12,7 @@ class HomePageView(View):
     def get(self,request,*args,**kwargs): 
         
             
-        data =NobelPrize.objects.values('category','laureate__born_country' ).annotate(Count('category'))
+        data =NobelPrize.objects.values('category','laureate__born_country').annotate(Count('category'))
        
         print(data)
             
@@ -23,3 +23,41 @@ class HomePageView(View):
         }
         
         return render(request,"core_section/home.html",context)
+
+class NobelPrizeGender(View):
+    model=Laureate,NobelPrize
+    template_name='core_section/gender_based_count.html'
+    def get(self,request,*args,**kwargs):      
+            
+        gender_count =NobelPrize.objects.filter(year__range=["1990-01-01", "2000-01-31"]).values('category','laureate__gender','year','category').annotate(Count('year'))
+        print(gender_count)
+
+        context={
+            "gender_count":gender_count
+           
+
+        }
+        
+        return render(request,"core_section/gender_based_count.html",context)
+
+class AffiliationCount(View):
+    count={}
+
+    model=Laureate,NobelPrize
+    template_name='core_section/gender_based_count.html'
+    def get(self,request,*args,**kwargs): 
+         
+            
+        affiliation_count =NobelPrize.objects.values('category','laureate__affiliation',).annotate(Count('laureate__affiliation'),Count('category')).order_by('-laureate__affiliation__count','-category')[:5]
+        
+        
+
+        
+        context={
+            "affiliation_count":affiliation_count
+           }
+        
+        return render(request,"core_section/count_affiliation.html",context)
+
+
+
