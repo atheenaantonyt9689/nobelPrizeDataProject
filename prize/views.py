65ca935628ea45e0fdc20c1cd34a531aa755e15b
atheenaantonyt9689 <atheenaantony8@gmail.com>
2021-06-04 10:42:20 +0530

    count of prizess per university complted

--- prize/views.py
+++ prize/views.py
@@ -12,7 +12,7 @@ class HomePageView(View):
     def get(self,request,*args,**kwargs): 
         
             
-        data =NobelPrize.objects.values('category','laureate__born_country').annotate(Count('category'))
+        data =NobelPrize.objects.values('category','laureate__born_country').annotate(Count('category'),Count('laureate__born_country')).distinct()
        
         print(data)
             
@@ -29,11 +29,13 @@ class NobelPrizeGender(View):
     template_name='core_section/gender_based_count.html'
     def get(self,request,*args,**kwargs):      
             
-        gender_count =NobelPrize.objects.filter(year__range=["1990-01-01", "2000-01-31"]).values('category','laureate__gender','year','category').annotate(Count('year'))
-        print(gender_count)
+        gender_count1 =NobelPrize.objects.filter(year__range=["1990-01-01", "2000-01-1"]).values('category','laureate__gender','year','category').annotate(Count('category'),Count('laureate__gender'))
+        gender_count2 =NobelPrize.objects.filter(year__range=["2000-01-01", "2000-01-31"]).values('category','laureate__gender','year','category').annotate(Count('category'))
+
+        print(gender_count1)
 
         context={
-            "gender_count":gender_count
+            "gender_count1":gender_count1
            
 
         }
@@ -41,15 +43,12 @@ class NobelPrizeGender(View):
         return render(request,"core_section/gender_based_count.html",context)
 
 class AffiliationCount(View):
-    count={}
-
     model=Laureate,NobelPrize
-    template_name='core_section/gender_based_count.html'
-    def get(self,request,*args,**kwargs): 
-         
-            
-        affiliation_count =NobelPrize.objects.values('category','laureate__affiliation',).annotate(Count('laureate__affiliation'),Count('category')).order_by('-laureate__affiliation__count','-category')[:5]
-        
+    template_name='core_section/count_affiliation.html'
+    def get(self,request,*args,**kwargs):            
+        affiliation_count =NobelPrize.objects.values('laureate__affiliation',).annotate(Count('id')).order_by('-id__count')[:5]
+               
+        print(affiliation_count)
         
 
         
